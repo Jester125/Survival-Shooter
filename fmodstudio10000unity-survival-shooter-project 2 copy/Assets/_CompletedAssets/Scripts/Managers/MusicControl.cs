6f8ce0440ee8ace3f348dd4d49460a70ace05a9f
Jesse Lawson <122029812+Jester125@users.noreply.github.com>
2023-11-17 15:51:14 +0000

    Added break beat
    and fixed issues

--- fmodstudio10000unity-survival-shooter-project 2 copy/Assets/_CompletedAssets/Scripts/Managers/MusicControl.cs
+++ fmodstudio10000unity-survival-shooter-project 2 copy/Assets/_CompletedAssets/Scripts/Managers/MusicControl.cs
@@ -10,16 +10,23 @@ public class MusicControl : MonoBehaviour
 
     //[FMODUnity.EventRef]
     public string music = "event:/Music/Music";
+    public string breakBeat = "event:/Music/Break";
 
     FMOD.Studio.EventInstance musicEv;
-    
+    FMOD.Studio.EventInstance breakEv;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         musicEv = FMODUnity.RuntimeManager.CreateInstance(music);
+        breakEv = FMODUnity.RuntimeManager.CreateInstance(breakBeat);
 
         musicEv.start();
+        breakEv.start();
+
     }
 
     // Update is called once per frame
